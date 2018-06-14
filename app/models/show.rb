544581115643b679cddff4836e5dc1d19068f8eb
Richard Melchior <raustin.melchior@gmail.com>
2018-06-14 19:31:09 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -12,15 +12,15 @@ class Show < ActiveRecord::Base
   end
 
   def self.least_popular_show
-
+    Show.where(self.lowest_rating).take
   end
 
   def self.ratings_sum
-
+    Show.sum("rating")
   end
 
   def self.popular_shows
-
+    Show.all.where("rating > ?", 5)
   end
 
   def self.shows_by_alphabetical_order
